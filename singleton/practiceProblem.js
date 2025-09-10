3265bbd3f5bab03f2e5ec0bda64b609ff1e392cf
Guichardx2 <jeancharles.g.guichard@gmail.com>
2025-09-10 19:58:23 -0300

    singleton 2

--- singleton/practiceProblem.js
+++ singleton/practiceProblem.js
@@ -5,19 +5,29 @@ class Database {
       return Database.instance;
     }
 
+    this.connectionHistorty = [];
     this.connectionString = connectionString;
     this.id = Math.random();
+    Database.instance = this;
     console.log(`🔌 Nova conexão criada: ${this.id}`);
   }
 
   query(sql) {
     console.log(`Executando query [${sql}] na conexão ${this.id}`);
+    this.connectionHistorty.push(sql);
   }
+
+  logHistory() {
+    console.log(`Histórico de queries na conexão ${this.id}:`, this.connectionHistorty);
+  }
+
 }
 
 // Cliente cria várias conexões (mesmo que não precise)
 const db1 = new Database("db://meu-banco");
 db1.query("SELECT * FROM users");
+db1.logHistory();
 
 const db2 = new Database("db://meu-banco");
 db2.query("SELECT * FROM products");
+db2.logHistory();
