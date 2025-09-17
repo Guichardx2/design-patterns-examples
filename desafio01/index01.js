a60af11673839861c8b421eed7a73f5b594da031
Guichardx2 <jeancharles.g.guichard@gmail.com>
2025-09-17 19:30:18 -0300

    Soluções

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,48 @@
-console.log("Hello, World! 001");
+//Solução Singleton Pattern
+
+class Config {
+    //Construtor com lógica de singleton instanciando a própria classe
+  constructor() {
+
+    if (Config.instance) {
+      return Config.instance;
+    }
+
+    this.settings = {};
+    this.configHistory = []; // Variável de histórico para garantir que todas as instâncias compartilhem o mesmo histórico
+    Config.instance = this;
+    console.log("🔧 Nova instância de Config criada");
+  }
+
+  setConfig(key, value) {
+    this.settings[key] = value;
+    this.configHistory.push({ key, value });
+  }
+
+  getConfig(key) {
+
+    if (!(key in this.settings)) {
+      return `Configuração ${key} não encontrada`;
+    }
+
+    return this.settings[key];
+  }
+
+  //Nova função para mostrar o histórico de configurações
+  showConfigHistory() {
+    console.log("Histórico de configurações:", this.configHistory);
+  }
+}
+
+// Cliente 1
+const config1 = new Config();
+config1.setConfig("lang", "pt-BR");
+console.log(config1.getConfig("lang"));
+console.log(config1.getConfig("teste")); // Testando chave inexistente
+config1.showConfigHistory();
+
+// Cliente 2
+const config2 = new Config();
+config2.setConfig("theme", "dark");
+console.log(config2.getConfig("theme"));
+config2.showConfigHistory();
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,55 @@
-console.log("Hello, World! 002");
+// Solução Factory Method Pattern
+
+//Classes concretas
+class Bicicleta {
+  move() {
+    return "Pedalando a bicicleta no pátio da UNISATC";
+  }
+}
+
+class Patinete {
+  move() {
+    return "Andando de patinete pelo estacionamento da UNISATC";
+  }
+}
+
+class Onibus {
+  move() {
+    return "Andando de ônibus e chegando na UNISATC";
+  }
+}
+
+// Factory para as classes concretas de transporte
+class TransportFactory {
+  static types = {
+    bicicleta: Bicicleta,
+    patinete: Patinete,
+    onibus: Onibus,
+  };
+
+  static createTransport(type) {
+    const TransportClass = this.types[type];
+    if (!TransportClass) {
+      throw new Error("Tipo de transporte não suportado");
+    }
+    return new TransportClass();
+  }
+}
+
+//Cliente
+function main() {
+  const types = ["bicicleta", "patinete", "onibus"];
+
+  let transport;
+
+  try {
+    types.forEach((type) => {
+      transport = TransportFactory.createTransport(type);
+      console.log(transport.move());
+    });
+  } catch (e) {
+    throw new Error("Erro ao processar transporte", e.message);
+  }
+}
+
+main();
