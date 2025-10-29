7ffc445c9addf94ff3dd77752606e3b71ed7acea
Guichardx2 <jeancharles.g.guichard@gmail.com>
2025-10-29 19:42:24 -0300

    Prova 02

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,48 @@
-console.log("Hello, World! 001");
+//Decorator Pattern
+class Message {
+    constructor(text) {
+      this.text = text;
+    }
+  
+    getText() {
+      return this.text;
+    }
+  }
+  
+  class MessageDecorator {
+    constructor(message) {
+      this.message = message;
+    }
+  
+    getText() {
+      return this.message.getText();
+    }
+  }
+  
+  class UpperCaseDecorator extends MessageDecorator {
+    getText() {
+      return super.getText().toUpperCase();
+    }
+  }
+  
+  class ExclamationDecorator extends MessageDecorator {
+    getText() {
+      return super.getText() + "!!!";
+    }
+  }
+  
+  class EmojiDecorator extends MessageDecorator {
+    getText() {
+      return "😊 " + super.getText() + " 😊";
+    }
+  }
+  
+  const msg = new Message("hoje o dia está horrível");
+  console.log(msg.getText());
+  
+  const decoratedMsg = new EmojiDecorator(
+    new ExclamationDecorator(
+      new UpperCaseDecorator(msg)
+    )
+  );
+  console.log(decoratedMsg.getText()); // 😊 HOJE O DIA ESTÁ HORRÍVEL!!! 😊
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,63 @@
-console.log("Hello, World! 002");
+//Bridge Pattern
+class TV {
+    turnOn() {
+      console.log("TV ligada.");
+    }
+    turnOff() {
+      console.log("TV desligada.");
+    }
+    setVolume(volume) {
+      console.log(`Volume da TV: ${volume}`);
+    }
+  }
+  
+  class Radio {
+    turnOn() {
+      console.log("Rádio ligado.");
+    }
+    turnOff() {
+      console.log("Rádio desligado.");
+    }
+    setVolume(volume) {
+      console.log(`Volume do Rádio: ${volume}`);
+    }
+  }
+  
+  class RemoteControl {
+    constructor(device) {
+      this.device = device;
+    }
+  
+    pressPowerButton(on) {
+      if (on) this.device.turnOn();
+      else this.device.turnOff();
+    }
+  
+    setVolume(volume) {
+      this.device.setVolume(volume);
+    }
+  }
+  
+  class AdvancedRemoteControl extends RemoteControl {
+    mute() {
+      console.log("Silenciando...");
+      this.device.setVolume(0);
+    }
+  }
+  
+  // Cliente
+  const tv = new TV();
+  const radio = new Radio();
+  
+  const remoteTV = new RemoteControl(tv);
+  remoteTV.pressPowerButton(true);
+  remoteTV.setVolume(50);
+  remoteTV.pressPowerButton(false);
+  
+  console.log("---");
+  
+  const remoteRadio = new AdvancedRemoteControl(radio);
+  remoteRadio.pressPowerButton(true);
+  remoteRadio.setVolume(30);
+  remoteRadio.mute();
+  remoteRadio.pressPowerButton(false);
